897b8e63dc448d353e2d585ef36e65a73d430806
Robin Doirin <robindoirin@gmail.com>
2019-07-08 14:59:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
-function functionName() {
-
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+    for (let i=0; i < lis.length; i++) {
+        lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+    };
 }
